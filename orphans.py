0d414924c14a70c45b5b687683783d53c995da42
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-02-15 17:38:52 +0300

    Add files via upload

---
+++ orphans.py
@@ -0,0 +1,17 @@
+import os 
+os.system ("clear")
+name = input ("Enter the account name:")
+print (name)
+v7x = input ("Enter the guess file:")
+print (v7x)
+ss = input ("Enter information about the person:")
+print (ss)
+print ("The information is being analyzed. Wait a few moments")
+list100=0
+while list100 < 100000000 :
+	list100=list100+1
+	print (list100)
+print ("The password was obtained successfully")
+print ("Please wait a few moments for the configuration process to complete")
+print ("=====>> taesty100638$62")
+
